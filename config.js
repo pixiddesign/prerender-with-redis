49371b7c8505e71b86e82740191603e241d9adaf
Walter Franchetti <walter.franchetti@gmail.com>
2015-03-20 12:35:59 -0300

    config

--- config.js
+++ config.js
@@ -13,14 +13,14 @@ exports = module.exports = function () {
 	if ((env.PRERENDER_LOGGER && env.PRERENDER_LOGGER.toString() !== '' && env.PRERENDER_LOGGER.toString().toLowerCase() !== 'false')) {
 		config.logger = true;
 	}
-	if ((env.BASIC_AUTH_USERNAME && env.BASIC_AUTH_USERNAME !== '') && (env.BASIC_AUTH_PASSWORD && env.BASIC_AUTH_PASSWORD !== '')) {
+	if ((env.BASIC_AUTH_USERNAME && env.BASIC_AUTH_USERNAME.toString() !== '' && env.BASIC_AUTH_USERNAME.toString().toLowerCase() !== 'false') && (env.BASIC_AUTH_PASSWORD && env.BASIC_AUTH_PASSWORD.toString() !== '' && env.BASIC_AUTH_PASSWORD.toString().toLowerCase() !== 'false')) {
 		config.auth = true;
 	}
-	if ((env.AWS_ACCESS_KEY_ID && env.AWS_ACCESS_KEY_ID !== '') && (env.AWS_SECRET_ACCESS_KEY && env.AWS_SECRET_ACCESS_KEY !== '') && (env.S3_BUCKET_NAME && env.S3_BUCKET_NAME !== '')) {
+	if ((env.AWS_ACCESS_KEY_ID && env.AWS_ACCESS_KEY_ID.toString() !== '' && env.AWS_ACCESS_KEY_ID.toString().toLowerCase() !== 'false') && (env.AWS_SECRET_ACCESS_KEY && env.AWS_SECRET_ACCESS_KEY.toString() !== '' && env.AWS_ACCESS_KEY_ID.toString().toLowerCase() !== 'false') && (env.S3_BUCKET_NAME && env.S3_BUCKET_NAME.toString() !== '' && env.S3_BUCKET_NAME.toString().toLowerCase() !== 'false')) {
 		config.s3HtmlCache = true;
 	}
 
-	if ((env.REDISTOGO_URL && env.REDISTOGO_URL !== '') || (env.REDISCLOUD_URL && env.REDISCLOUD_URL !== '') || (env.REDISGREEN_URL && env.REDISGREEN_URL !== '') || (env.REDIS_URL && env.REDIS_URL !== '')) {
+	if ((env.REDISTOGO_URL && env.REDISTOGO_URL.toString() !== '' && env.REDISTOGO_URL.toString().toLowerCase() !== 'false') || (env.REDISCLOUD_URL && env.REDISCLOUD_URL.toString() !== '' && env.REDISTOGO_URL.toString().toLowerCase() !== 'false') || (env.REDISGREEN_URL && env.REDISGREEN_URL.toString() !== '' && env.BREDISGREEN_URL.toString().toLowerCase() !== 'false') || (env.REDIS_URL && env.REDIS_URL.toString() !== '' && env.REDIS_URL.toString().toLowerCase() !== 'false')) {
 		config.redis = true;
 	}
 
